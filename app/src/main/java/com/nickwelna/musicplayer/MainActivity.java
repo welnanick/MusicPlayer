bc14858c2bbae7b6a2a560c66a682bc1e5d8faed
Nick Welna <welnanick@gmail.com>
2017-12-08 15:23:29 -0800

    master: initial commit

---
+++ app/src/main/java/com/nickwelna/musicplayer/MainActivity.java
@@ -0,0 +1,98 @@
+package com.nickwelna.musicplayer;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    float currentVolume = 0.5f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.bensound_dubstep);
+
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                playMusic();
+
+            }
+
+        });
+
+        Button pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                pauseMusic();
+
+            }
+
+        });
+
+        Button volumeUp = findViewById(R.id.volume_up);
+        volumeUp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                currentVolume += 0.1f;
+                AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * currentVolume), 0);
+
+            }
+
+        });
+
+        Button volumeDown = findViewById(R.id.volume_down);
+        volumeDown.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                currentVolume -= 0.1f;
+                AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, (int) (audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC) * currentVolume), 0);
+
+            }
+
+        });
+
+    }
+
+    public void playMusic() {
+
+        mediaPlayer.start();
+
+    }
+
+    public void pauseMusic() {
+
+        mediaPlayer.pause();
+
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        mediaPlayer.release();
+
+    }
+
+}
